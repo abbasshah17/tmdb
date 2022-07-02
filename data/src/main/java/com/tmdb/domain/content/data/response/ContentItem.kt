92c3be57d7d015f2ddf6ca0b2446f45768123ee2
Abbas Anwer Ali <abbasshah17@gmail.com>
2022-07-03 00:41:59 +0500

    Adds serializing annotations for moshi.

--- data/src/main/java/com/tmdb/domain/content/data/response/ContentItem.kt
+++ data/src/main/java/com/tmdb/domain/content/data/response/ContentItem.kt
@@ -1,8 +1,22 @@
 package com.tmdb.domain.content.data.response
 
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
 data class ContentItem(
-    val poster: String,
-    val title: String,
-    val description: String,
+    @Json(name = "poster_path")
+    val poster: String?,
+
+    @Json(name = "title")
+    val title: String?,
+
+    @Json(name = "name")
+    val name: String?,
+
+    @Json(name = "overview")
+    val description: String?,
+
+    @Json(name = "media_type")
     val mediaType: String,
 )
