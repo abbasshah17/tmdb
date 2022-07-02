69626e6ccaa8a0341d5abea029f70f241d8bc1af
Abbas Anwer Ali <abbasshah17@gmail.com>
2022-07-02 15:43:17 +0500

    Added missing: View injected with viewmodel.

--- app/src/main/java/com/starzplay/MainActivity.kt
+++ app/src/main/java/com/starzplay/MainActivity.kt
@@ -1,20 +1,26 @@
 package com.starzplay
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.view.Menu
+import android.view.MenuItem
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
+import com.google.android.material.snackbar.Snackbar
 import com.starzplay.databinding.ActivityMainBinding
+import com.tmdb.domain.content.ContentViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+
+    private val viewModel: ContentViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
