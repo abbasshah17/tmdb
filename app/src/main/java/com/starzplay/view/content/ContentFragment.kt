a4d11c8eeecd095a5936920b03f12f3b3eae30fa
Abbas Anwer Ali <abbasshah17@gmail.com>
2022-07-02 17:22:38 +0500

    A basic and incomplete UI structure for the content layout.

---
+++ app/src/main/java/com/starzplay/view/content/ContentFragment.kt
@@ -0,0 +1,11 @@
+package com.starzplay.view.content
+
+import com.starzplay.R
+import com.starzplay.base.views.BaseFragment
+import com.starzplay.databinding.SearchContentLayoutBinding
+
+class ContentFragment: BaseFragment<SearchContentLayoutBinding>() {
+
+    override val layout: Int
+        get() = R.layout.search_content_layout
+}
