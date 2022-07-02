493dcc85f717234c8fa17a7f97274960f4bd3a62
Abbas Anwer Ali <abbasshah17@gmail.com>
2022-07-02 20:36:08 +0500

    Changed contract return type.

--- data/src/main/java/com/tmdb/domain/content/repos/ContentRepo.kt
+++ data/src/main/java/com/tmdb/domain/content/repos/ContentRepo.kt
@@ -2,13 +2,13 @@ package com.tmdb.domain.content.repos
 
 import com.aaa.clean.repo.Repository
 import com.tmdb.domain.content.data.request.ContentRequest
-import com.tmdb.domain.content.data.response.ContentItem
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class ContentRepo @Inject internal constructor(
 ): Repository(), ContentRepository {
-    override fun fetchMovies(contentRequest: ContentRequest): Flow<List<ContentItem>> {
+
+    override fun fetchMovies(contentRequest: ContentRequest): Flow<ContentApiResponse> = flow {
         TODO("Not yet implemented")
     }
 }
