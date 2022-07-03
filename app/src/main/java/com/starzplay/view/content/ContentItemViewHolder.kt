6a35e6ceee41644bf50b8aed054de1ec1e16aeb4
Abbas Anwer Ali <abbasshah17@gmail.com>
2022-07-03 21:14:25 +0500

    Handles navigation to Content Item Detail.

--- app/src/main/java/com/starzplay/view/content/ContentItemViewHolder.kt
+++ app/src/main/java/com/starzplay/view/content/ContentItemViewHolder.kt
@@ -1,5 +1,6 @@
 package com.starzplay.view.content
 
+import androidx.navigation.findNavController
 import com.starzplay.base.views.recycler.RecyclerViewHolder
 import com.starzplay.databinding.ContentItemLayoutBinding
 import com.tmdb.domain.content.data.response.ContentItem
@@ -11,5 +12,12 @@ class ContentItemViewHolder(
 ) {
     override fun bind(data: ContentItem) {
         binding.content = data
+
+        binding.root.setOnClickListener {
+            binding.content?.let {
+                val action = ContentFragmentDirections.contentDetail(it)
+                binding.root.findNavController().navigate(action)
+            }
+        }
     }
 }
